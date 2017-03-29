111810bfdcc57d248b839cfb5bbdc7e9e5fa35ba
Shaun Ellis <shaun@sdellis.com>
2017-03-29 10:22:04 -0400

    adds manifesto

---
+++ src/manifesto-react-mixins.js
@@ -0,0 +1,39 @@
+
+const ManifestoReactMixins = {
+
+  hello: function(){
+    console.log( "hello!" );
+  },
+
+  title: function(){
+    console.log( this.getLabel() );
+  },
+
+  mainSequence: function(){
+    // mainSequence is the one without an id (not ideal since it could have an id)
+    const s = this.getSequences()
+    const main_sequence = s.filter((seq) => seq.id !== 'undefined')
+    return main_sequence[0]
+  },
+
+  getCanvasMainThumb: function(canvas){
+    const images = canvas.getImages()
+    const services = images[0].getResource().getServices()
+    return services[0].id + '/full/400,/0/default.jpg'
+  },
+
+  posts: function(){
+    const s = this.mainSequence()
+    const canvases = s.getCanvases()
+    return canvases.map(canvas => ({
+      code: canvas.id,
+      caption: canvas.getLabel(),
+      likes: 0,
+      id: canvas.id,
+      display_src: this.getCanvasMainThumb(canvas)
+    }));
+  }
+
+}
+
+export default ManifestoReactMixins;
--- src/store.js
+++ src/store.js
@@ -1,12 +1,21 @@
 import { createStore, compose } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
+import mixins from './manifesto-react-mixins'
 
 // import the root reducer
 import rootReducer from './reducers/index'
 
 import comments from './data/comments'
 import posts from './data/posts'
+import manifesto from '../node_modules/manifesto.js/dist/server/manifesto.js'
+
+
+manifesto.loadManifest('http://digital.library.villanova.edu/Item/vudl:92879/Manifest').then(function (manifest) {
+  const m = manifesto.create(manifest)
+  // lets mix in some convenience methods into manifesto's instance
+  window.manifestation = Object.assign( m, mixins )
+});
 
 // create an object for the default data
 const defaultState = {
@@ -21,6 +30,7 @@ const enhancers = compose(
 const store = createStore(rootReducer, defaultState, enhancers)
 
 export const history = syncHistoryWithStore(browserHistory, store)
+export const manifestation = window.manifestation;
 
 if(module.hot) {
   module.hot.accept('./reducers', () => {
